15dfb04a9a37179a3b25e5a314527cedf3512d50
Firehead94 <Jjonscott@comcast.net>
2015-04-12 02:29:02 -0400

    Started on GUI and a side view
    added GUI components
    - hearts
    - armor
    - grid/ammo bar
    - new space skybox

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+
+    [SerializeField]
+    private string firstLevel;
+
+    public void loadGame()
+    {
+        Application.LoadLevel(firstLevel);    
+    }
+}
