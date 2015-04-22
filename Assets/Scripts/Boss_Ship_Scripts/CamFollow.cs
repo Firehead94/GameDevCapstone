1d85ddc1d64ee57f4f2c477a591146e10c0c7c5b
Jay Aubry <jay.aubry@gmail.com>
2015-04-21 21:46:53 -0400

    Boss ship player changes

---
+++ Assets/Scripts/Boss_Ship_Scripts/CamFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour 
+{
+	private Transform player;
+	 
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.Find ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 playerPosition = player.position;
+		playerPosition.z = transform.position.z;
+		transform.position = playerPosition;
+	}
+}
---
+++ Assets/Scripts/Boss_Ship_Scripts/Firing.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firing : MonoBehaviour 
+{
+	public Rigidbody bullet;
+	public float speed = 20f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire();
+		}
+	}
+
+	void Fire()
+	{
+		Rigidbody instantiateBullet = Instantiate (bullet,
+		                                          transform.position,
+		                                          transform.rotation)
+												  as Rigidbody;
+
+		instantiateBullet.velocity = transform.TransformDirection (new Vector3 (0, 0, speed));
+
+	}//end Fire
+}
